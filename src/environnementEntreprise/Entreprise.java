3afeeccf7620704a49ec5c7601582b3224e40743
AdamWizard <48995051+AdamWizard@users.noreply.github.com>
2021-05-12 17:47:09 +0200

    classes Entreprise, Departement et Employe commencés. (très rudimentaires il faudra surement compléter)

--- src/environnementEntreprise/Entreprise.java
+++ src/environnementEntreprise/Entreprise.java
@@ -1,5 +1,25 @@
 package environnementEntreprise;
 
 public class Entreprise {
+	//attributs
+	private String nom;
+	private Departement[] departements;
+	
+	//methodes
+	public Departement[] getDepartements() {
+		return departements;
+	}
+
+	public void setDepartements(Departement[] departements) {
+		this.departements = departements;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+
+	public void setName(String nom) {
+		this.nom = nom;
+	}
 
 }
