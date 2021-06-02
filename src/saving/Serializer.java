326239e0c524d7ed08fc4810f3bb43d8230b1446
Banalian <lilian.pouvreau@free.fr>
2021-06-02 16:30:11 +0200

    started serializer

--- src/saving/Serializer.java
+++ src/saving/Serializer.java
@@ -1,29 +1,46 @@
 package saving;
 
 import java.io.File;  // Import the File class
-import java.io.IOException;
+import java.io.IOException;// Import the IOException class to handle errors
 
-import environnementEntreprise.Company;  // Import the IOException class to handle errors
+import environnementEntreprise.Company;
+import pointeuse.SerialPointeuse;  
 
 
 public class Serializer {
 
-/*Save the Company's data, then access each dpt on the company's dpt list to save its data, 
- then access each employee on the dpt's list to save its data
- every time it creates another file for each class (company-dpt-employee) */
-	public void Serializer(Company company) {
+
+	public File createOpenFile(String fileName) {
+		
+		File nameFile = null;
 		
 		//create a file
 		try {
-		      File company = new File("filename.txt");
-		      if (myObj.createNewFile()) {
-		        System.out.println("File created: " + myObj.getName());
-		      } else {
-		        System.out.println("File already exists.");
-		      }
-		    } catch (IOException e) {
-		      System.out.println("An error occurred.");
-		      e.printStackTrace();
-		    }
+			nameFile = new File(fileName);
+			if (nameFile.createNewFile()) {
+				System.out.println("File created: " + nameFile.getName());
+			} else {
+				System.out.println("File already exists.");
+			}
+		} catch (IOException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+		
+		return nameFile;
 	}
+	
+	/*Save the Company's data, then access each dpt on the company's dpt list to save its data, 
+	 then access each employee on the dpt's list to save its data
+	 every time it creates another file for each class (company-dpt-employee) */
+	public void serializeCompany(Company company) {
+		
+		File compFile = createOpenFile("CompanyFile.txt");
+	}
+	
+	
+	public void serializePointeuseData(SerialPointeuse[] tabData) {
+		
+	}
+	
 }
