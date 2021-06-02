ec8d4ec9231049965e6337843229a84d2f188fa5
CorentinEsn <75800644+CorentinEsn@users.noreply.github.com>
2021-06-02 16:36:31 +0200

    blop
    blip blap bloup

--- src/saving/Serializer.java
+++ src/saving/Serializer.java
@@ -1,29 +1,30 @@
 package saving;
 
 import java.io.File;  // Import the File class
-import java.io.IOException;
+import java.io.IOException; // Import the IOException class to handle errors
 
-import environnementEntreprise.Company;  // Import the IOException class to handle errors
+import environnementEntreprise.Company;  
 
 
 public class Serializer {
 
-/*Save the Company's data, then access each dpt on the company's dpt list to save its data, 
+	/*Save the Company's data, then access each dpt on the company's dpt list to save its data, 
  then access each employee on the dpt's list to save its data
  every time it creates another file for each class (company-dpt-employee) */
 	public void Serializer(Company company) {
-		
-		//create a file
+
+		//create a file the same name of the company's
+
 		try {
-		      File company = new File("filename.txt");
-		      if (myObj.createNewFile()) {
-		        System.out.println("File created: " + myObj.getName());
-		      } else {
-		        System.out.println("File already exists.");
-		      }
-		    } catch (IOException e) {
-		      System.out.println("An error occurred.");
-		      e.printStackTrace();
-		    }
+			File companyFile = new File(company.getName()+".csv");
+			if (companyFile.createNewFile()) {
+				System.out.println("File created: " + companyFile.getName());
+			} else {
+				System.out.println("File already exists.");
+			}
+		} catch (IOException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
 	}
 }
